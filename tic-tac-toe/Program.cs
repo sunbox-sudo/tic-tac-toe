6c8dde4e30419c8cbfe4e6154c2f29104d3612fe
sunbox-sudo <sunbox_@outlook.com>
2025-11-28 09:04:39 +0100

    added som methods eand region so i have a structure

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace tic_tac_toe
+﻿using System.Diagnostics;
+
+namespace tic_tac_toe
 {
     internal class Program
     {
+        char[,] Board = new char[3, 3];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Debug.WriteLine("Tic Tac Toe Game Started");
+
         }
+
+        #region Game 
+        static void Setup_Board(char[,] Board) 
+        {
+            
+        }
+
+        #endregion
+
+
+        #region UI Methods
+
+        static void Print_Board(char[,] Board) 
+        { 
+            
+        }
+
+        #endregion
+
     }
 }
